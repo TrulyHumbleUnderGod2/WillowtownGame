da4ff3522d86fb4dbb731d106a71e3453411cf1c
e6kwon <kwone@students.dsbn.org>
2024-10-09 10:44:45 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,17 @@
+import sys
 import pygame
 
+pygame.init()
 
+screen = pygame.display.set_mode((640,320))
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(60)

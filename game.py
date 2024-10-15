1df80981c31c579eb85c2ea19a7a1483f1871cc0
e6kwon <kwone@students.dsbn.org>
2024-10-15 11:09:11 -0400

    Update game.py

--- game.py
+++ game.py
@@ -3,9 +3,8 @@ import pygame
 
 # pygame setup
 pygame.init()
-
-screen = pygame.display.set_mode((640,320))
-
+screen = pygame.display.set_mode((640, 320))
+pygame.display.toggle_fullscreen()
 clock = pygame.time.Clock()
 running = True
 dt = -0
@@ -17,8 +16,29 @@ while running:
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            running = False
 
-    pygame.display.update()
-    clock.tick(60)
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("white")
+
+    pygame.draw.circle(screen, "red", player_pos, 10)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
